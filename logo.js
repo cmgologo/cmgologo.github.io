69ee68449ae192ffb560cd63843cf6524c296f11
Christo <cmgologo@gmail.com>
2018-03-11 14:20:12 +0200

    save2

---
+++ logo.js
@@ -0,0 +1,75 @@
+if (screen.width <= 699) {
+document.location = "mobi.html";
+}
+
+function wu(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "block";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function home(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "block";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function about(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "block";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function contact(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "block";
+}
---
+++ logom.js
@@ -0,0 +1,71 @@
+function wu(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "block";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function home(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "block";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function about(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "block";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "none";
+}
+
+function contact(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+
+	var pfour =
+	document.getElementById("pconfour");
+	pfour.style.display = "block";
+}
